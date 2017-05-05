acb2d83f2d3f18aacf248389da0fb5ed9b5a8031
Patryk Olewniak <thawar@gmail.com>
2017-05-05 15:31:08 +0200

    added a second table with polish word db creation

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 class Database:
-
     @staticmethod
     def create_database():
         try:
@@ -21,15 +20,28 @@ class Database:
                     mastered             INTEGER (1)    DEFAULT (0),
                     difficult            INTEGER (2)    DEFAULT (1),
                     synonyms             VARCHAR (2056),
-                    partofspeech         VARCHAR (256)
-                )"""
-                           )
+                    partofspeech         VARCHAR (256));
+                    """)
+
+            cursor.execute("""
+                            CREATE TABLE polishWords (
+                                id_ENG               INTEGER        PRIMARY KEY AUTOINCREMENT UNIQUE,
+                                word                 VARCHAR (80)  NOT NULL,
+                                description          VARCHAR (1024),
+                                category             VARCHAR (128),
+                                importance           INTEGER (2)    DEFAULT (0),
+                                creationDate         DATETIME       DEFAULT ( (DATETIME('now') ) ),
+                                lastViewdate         DATETIME,
+                                viewcount            INTEGER (4)    DEFAULT (0),
+                                correctanswers       INTEGER (4)    DEFAULT (0),
+                                mastered             INTEGER (1)    DEFAULT (0),
+                                difficult            INTEGER (2)    DEFAULT (1),
+                                synonyms             VARCHAR (2056),
+                                partofspeech         VARCHAR (256));
+                                """)
             db.close()
         except sqlite3.OperationalError:
             print ("Table exist")
-
-
-
 
 
 
