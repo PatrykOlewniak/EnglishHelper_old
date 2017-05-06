d70f8becc57448f3015f1da9ce9e9b8f8d42576c
Patryk Olewniak <thawar@gmail.com>
2017-05-06 11:44:44 +0200

    app exec

--- englishHelper.py
+++ englishHelper.py
@@ -3,10 +3,18 @@ from PyQt5.QtWidgets import QApplication, QDialog
 from mainLayout import Ui_Dialog
 
 
+class EnglishHelperMainWindow(QDialog):
+    def __init__ (self, parent=None):
+        super(EnglishHelperMainWindow, self).__init__(parent)
+        self.ui_glowne = Ui_Dialog()
+        self.ui_glowne.setupUi(self)
 
 
-app = QApplication(sys.argv)
-window = QDialog()
-ui = Ui_Dialog()
-ui.setupUi(window)
-sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = EnglishHelperMainWindow()
+    form.show()
+    #app.setQuitOnLastWindowClosed( False)  # by zamykalo okno w odp kolejnosci, likwiduje "QObject::startTimer: QTimer can only be used with threads started" ... blad
+    sys.exit(app.exec_())
