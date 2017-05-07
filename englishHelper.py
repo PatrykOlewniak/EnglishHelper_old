1a824b4c9da11baba4867a57015eab39caa01ec7
thavar <thawar@gmail.com>
2017-05-07 20:42:06 +0200

    little db rebuild, button connected

--- englishHelper.py
+++ englishHelper.py
@@ -10,8 +10,8 @@ class EnglishHelper(QDialog):
 
     def __init__ (self, parent=None):
         super(EnglishHelper, self).__init__(parent)
-        self.ui_glowne = Ui_Dialog()
-        self.ui_glowne.setupUi(self)
+        #self.ui_glowne = Ui_Dialog()
+        #self.ui_glowne.setupUi(self)
         EHdatabase=Database()
         EHdatabase.connection()
         self.button_connections()
@@ -26,16 +26,30 @@ class EnglishHelper(QDialog):
 
         self.db.commit()
 
+
+
     def button_connections(self):
         self.ui_glowne.shuffleButton.clicked.connect(self.shufle_word_from_DB)
         self.ui_glowne.checkButton.clicked.connect(self.checked_pressed_func)
+        self.ui_glowne.pushButton.clicked.connect(self.add_button_pressed_func)
 
     def shufle_word_from_DB(self):
-        print("shufled word")
+        self.ui_glowne.kategoriaComboBox_2.addItem("kkk")
+        category = self.ui_glowne.kategoriaComboBox_2.currentText()
+        print(category)
 
     def checked_pressed_func(self):
         print("CHECKED PRESSED")
+        self.insert_to_english_Words()
 
+    def add_button_pressed_func(self):
+        print("BUTTON ADD PRESSED")
+
+    def comboBoxes_get_values(self):
+        self.cursor.execute("SELECT category from englishWords")
+        self.db.commit()
+        k=self.cursor.fetchall()
+        print(k)
 
 
 
